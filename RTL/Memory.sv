8b861e11b184d94bab0e025613c11eeb37c4a0ff
PraisedChild <132355393+PraisedChild@users.noreply.github.com>
2024-09-25 22:39:13 +0300

    Add files via upload

---
+++ RTL/Memory.sv
@@ -0,0 +1,118 @@
+module Memory (
+    input HWRITE,HCLK,HRESETn,HREADY_Prev,HSEL1,HBURST,HTRANS,
+    input [30:0]HADDR,
+    input [31:0]HWDATA,
+    output reg [31:0]HRDATA,
+    output reg HREADY
+);
+    //we will only use 1 slave, but will keep the other as dummy to use the MUX and Decoder
+    
+    reg [31:0] mem1 [30:0];
+
+    typedef enum bit [2:0] {
+        IDLE = 3'b001,
+        ACTIVE_READ = 3'b010,
+        ACTIVE_WRITE = 3'b100
+    } state_e;
+
+    state_e cs,ns;
+    reg [30:0] ADDR;
+    register add_reg (.D(HADDR),.CLK(HCLK),.Q(ADDR));
+
+    always @(posedge HCLK or negedge HRESETn) begin
+            if(!HRESETn)begin
+                cs<=IDLE;
+            end
+            else
+                cs<=ns;
+    end
+
+    always@(*)begin
+        case(cs)
+        IDLE:begin
+            HRDATA=0;
+            HREADY=1;
+        end
+        ACTIVE_READ:begin
+            HRDATA = mem1[ADDR];
+            HREADY = 1;
+        end
+        ACTIVE_WRITE:begin
+            HREADY = 1;
+            HRDATA = 0;
+        end
+        default:begin
+            HRDATA = 0;
+            HREADY = 1;
+        end 
+        endcase
+    end
+
+    always@(posedge HCLK)begin
+        if(HSEL1)begin
+            if(cs == ACTIVE_WRITE) begin
+                mem1 [ADDR] = HWDATA;
+            end
+        end
+    end
+
+    always@(*)begin
+        case(cs)
+        IDLE:begin
+            if(!HTRANS)begin
+                ns = IDLE;
+            end
+            else begin
+                if(HWRITE)begin
+                    ns = ACTIVE_WRITE;
+                end
+                else
+                    ns = ACTIVE_READ;
+            end
+        end
+        ACTIVE_READ:begin
+            if(HBURST)
+                ns = ACTIVE_READ;
+            else begin
+                if(!HTRANS)
+                    ns = IDLE;
+                else begin
+                    if(HWRITE)
+                        ns = ACTIVE_WRITE;
+                    else 
+                        ns = ACTIVE_READ;
+                end
+            end
+            
+        end
+        ACTIVE_WRITE:begin
+            if(HBURST)
+                ns = ACTIVE_WRITE;
+            else begin
+                if(!HTRANS)
+                    ns = IDLE;
+                else begin
+                    if(HWRITE)
+                        ns = ACTIVE_WRITE;
+                    else 
+                        ns = ACTIVE_READ;
+                end
+            end
+        end
+        default:begin
+            ns = IDLE;
+        end
+        endcase
+    end
+
+endmodule
+
+module register (
+    input [30:0] D,
+    input CLK,
+    output reg [30:0]Q
+);
+    always@(posedge CLK)begin
+        Q<=D;
+    end
+endmodule
---
+++ RTL/SYSTEM.v
@@ -0,0 +1,68 @@
+module SYSTEM (
+    input [31:0] DATA,OPCODE,RESPONSE,
+    input HCLK,HRESETn,
+    output [4:0]RESPONSE_ADDR,
+    output REG_ENABLE,REG_WRITE,DONE,WAIT
+);
+    wire HREADY,HWRITE,HTRANS,HBURST,HSEL1,HSEL2,MUX_SEL,HRDATA_1,HRDATA_2,HREADYOUT_1,HREADYOUT_2;
+    wire [31:0] HWDATA,HADDR,HRDATA;
+
+    AHB_MASTER mstr (
+        .HREADY(HREADY),
+        .HWDATA(HWDATA),
+        .HADDR(HADDR),
+        .HWRITE(HWRITE),
+        .HTRANS(HTRANS),
+        .HBURST(HBURST),
+        .HCLK(HCLK),
+        .HRESETn(HRESETn),
+        .DATA(DATA),
+        .OPCODE(OPCODE),
+        .RESPONSE(RESPONSE),
+        .RESPONSE_ADDR(RESPONSE_ADDR),
+        .REG_ENABLE(REG_ENABLE),
+        .REG_WRITE(REG_WRITE),
+        .DONE(DONE),
+        .WAIT(WAIT)
+        );
+    Memory mem_slave (
+        .HWRITE(HWRITE),
+        .HCLK(HCLK),
+        .HRESETn(HRESETn),
+        .HSEL1(HSEL1),
+        .HBURST(HBURST),
+        .HTRANS(HTRANS),
+        .HADDR(HADDR),
+        .HWDATA(HWDATA),
+        .HRDATA(HRDATA_1),
+        .HREADY(HREADY_1)
+        );
+    UART dummy_slave (
+        .HWRITE(HWRITE),
+        .HCLK(HCLK),
+        .HRESETn(HRESETn),
+        .HSEL1(HSEL2),
+        .HBURST(HBURST),
+        .HTRANS(HTRANS),
+        .HADDR(HADDR),
+        .HWDATA(HWDATA),
+        .HRDATA(HRDATA_2),
+        .HREADY(HREADY_2)
+        );
+    AHB_Decoder decode (
+        .HADDR(HADDR[31]),
+        .HSEL_1(HSEL1),
+        .HSEL_2(HSEL2),
+        .MUX_SEL(MUX_SEL)
+        );
+    AHB_MUX mux (
+        .HRDATA_1(HRDATA_1),
+        .HRDATA_2(HRDATA_2),
+        .HREADYOUT_1(HREADY_1),
+        .HREADYOUT_2(HREADY_2),
+        .MUX_SEL(MUX_SEL),
+        .HRDATA(HRDATA),
+        .HREADY(HREADY)
+        );
+    
+endmodule
